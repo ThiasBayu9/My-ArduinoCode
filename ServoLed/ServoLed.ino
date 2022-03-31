a8e63565778c3c09ab02f682b86fd466a057527d
ThiasBayu9 <74868069+ThiasBayu9@users.noreply.github.com>
2022-03-31 11:00:18 +0800

    Add Servo LED

---
+++ ServoLed/ServoLed.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+Servo reee;
+int pos = 00;
+int buzz = 13;
+int led0 = 7;
+int led1 = 5;
+
+void setup() {
+  reee.attach(3);
+  pinMode(buzz, OUTPUT);
+  pinMode(led0, OUTPUT);
+  pinMode(led1, OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(led0, HIGH);
+  digitalWrite(led1, LOW);
+  delay(1000);
+
+  for(led0 == HIGH;){
+    digitalWrite(buzz, HIGH);
+    delay(500);
+    }
+  digitalWrite(led0, LOW);
+  digitalWrite(led1, HIGH);
+  delay(1000);
+}
